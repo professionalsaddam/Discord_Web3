250efaea5c20099ad75bb759a223abdc11ad4c0c
Saddam Hussain <syncorb2022@gmail.com>
2024-08-04 14:59:48 +0530

    Listing Channels and Joining User to a channel

--- src/App.js
+++ src/App.js
@@ -25,6 +25,9 @@ function App() {
 
   const [dappcord, setDappcord] = useState(null);
 
+  const [channels,setChannels] = useState([]);
+  const [currentChannel,setCurrentChannel] = useState(null);
+
   const loadBlockchain = async () => {
 
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -37,6 +40,19 @@ function App() {
     setDappcord(dappcord);
 
     console.log("Dappcord Contract",dappcord.address);
+
+    const totalChannels = await dappcord.totalChannels();
+    const channels = []
+
+    for (var i = 1; i <= totalChannels; i++) {
+      const channel = await dappcord.getChannel(i)
+      channels.push(channel)
+    }
+
+    setChannels(channels)
+
+    console.log(channels)
+
 
     window.ethereum.on("accountsChanged", () => {
       window.location.reload();
@@ -61,7 +77,15 @@ function App() {
       <main>
 
         <Servers />
-        <Channels />
+        <Channels
+
+        provider={provider}
+        account={account}
+        dappcord={dappcord}
+        channels={channels}
+        currentChannel={currentChannel}
+        setCurrentChannel={setCurrentChannel}
+        />
         <Messages />
 
       </main>
--- src/components/Channels.js
+++ src/components/Channels.js
@@ -1,10 +1,42 @@
 const Channels = ({ provider, account, dappcord, channels, currentChannel, setCurrentChannel }) => {
 
+  const channelHandler = async (channel) => {
+
+    const signer = await provider.getSigner();
+
+    const hasJoined = await dappcord.hasJoined(channel.id,account);
+
+
+    if(hasJoined)
+    {
+      console.log("Joined")
+      setCurrentChannel(channel)
+
+    }
+    else
+    {
+      const transaction = await dappcord.connect(signer).mint(channel.id, {value: channel.cost})
+      await transaction.wait();
+
+      setCurrentChannel(channel)
+
+    }
+
+  }
+
   return (
     <div className="channels">
       <div className="channels__text">
         <h2>Text Channels</h2>
-
+        <ul>
+        {channels.map((channel,index) => (
+            <li
+              key={index}
+              onClick={() => channelHandler(channel)}
+              className={currentChannel && currentChannel.id.toString() === channel.id.toString() ? "active" : ""}
+              >{channel.name}</li>
+         )) }
+        </ul>
       </div>
 
       <div className="channels__voice">
