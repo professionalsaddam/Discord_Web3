35a273d77fc3b37a4da56927f5556fd4c5f96e2e
Saddam Hussain <syncorb2022@gmail.com>
2024-08-04 12:33:09 +0530

    Inherited ERC721 (NFT) and Running Basic Test

--- contracts/Dappcord.sol
+++ contracts/Dappcord.sol
@@ -1,4 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+contract Dappcord is ERC721 {
 
-contract Dappcord {}
+
+    constructor(string memory _name, string memory _symbol)
+    ERC721(_name, _symbol)
+    {
+   
+    }
+
+}
--- test/Dappcord.js
+++ test/Dappcord.js
@@ -6,4 +6,30 @@ const tokens = (n) => {
 
 describe("Dappcord", function () {
 
+    const NAME = "Dappcord"
+    const SYMBOL = "DC"
+
+  describe('Deployment', () => { 
+
+      beforeEach(async () => {
+       
+        // Deploy contract
+        const Dappcord = await ethers.getContractFactory("Dappcord")
+        dappcord = await Dappcord.deploy(NAME, SYMBOL)
+    
+     
+      })
+
+      it("Sets the name", async () => {
+        const result = await dappcord.name()
+        expect(result).to.equal(NAME)
+      })
+
+      it("Sets the symbol", async () => {
+        const result = await dappcord.symbol()
+        expect(result).to.equal(SYMBOL)
+      })
+
+   })
+
 })
